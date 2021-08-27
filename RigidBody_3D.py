fix rotation extraction method

RigidBody_3D.py
@@ -3,13 +3,14 @@ import numpy as np
 from numpy.linalg import inv
 from scipy.linalg import sqrtm, norm
 from scipy.spatial.transform import Rotation
+import time
 
 ti.init(arch=ti.gpu)
 
 X    = ti.Vector.field(3, ti.f32, shape=12)
 P    = ti.Vector.field(3, ti.f32, shape=12)
 V    = ti.Vector.field(3, ti.f32, shape=12)
-view = ti.Vector.field(2, ti.f32, shape=12)
+view = ti.Vector.field(2, ti.f32, shape=14)
 edges = [
     [0,0,0,1,1,2,2,3,4,4,5,6, 1,1,2,3,4,2, 8,9,10,11, 8],
     [1,3,4,2,5,3,6,7,5,7,6,7, 3,4,5,4,6,7, 9,10,11,8, 10]
@@ -24,7 +25,7 @@ CM = ti.Vector.field(3, ti.f32, shape=())
 Q  = ti.Vector.field(3, ti.f32, shape=8)
 Q0 = ti.Vector.field(3, ti.f32, shape=8)
 Apq = ti.Matrix.field(3, 3, dtype=ti.f32, shape=())
-Aqq = ti.Matrix.field(3, 3, dtype=ti.f32, shape=())
+Aqq = ti.Matrix.field(3, 3, dtype=ti.f32, shape=())  # not used
 R   = ti.Matrix.field(3, 3, dtype=ti.f32, shape=())
 ALPHA = ti.field(ti.f32, shape=())
 ALPHA[None] = 1.
@@ -47,6 +48,8 @@ def initCamera():
 def init():
     V.fill(0)
     P.fill(0)
+    Apq.fill(0)
+    R.fill(0)
     # cube
     X[0] = 0.4, 0.6, 0.4
     X[1] = 0.4, 0.6, 0.6
@@ -61,7 +64,7 @@ def init():
     updateQ()
     initQ0()
     calcApq()
-    initAqq()
+    #initAqq()
     #floor
     X[8]  = 0., 0., 0.
     X[9]  = 0., 0., 1.
@@ -104,52 +107,76 @@ def calcApq():
     ])
     for i in ti.static(range(8)):
         A += Q[i] @ Q0[i].transpose()
+        # A[0,0] += Q0[i][0] * Q[i][0]
+        # A[1,0] += Q0[i][0] * Q[i][1]
+        # A[2,0] += Q0[i][0] * Q[i][2]
+
+        # A[0,1] += Q0[i][1] * Q[i][0]
+        # A[1,1] += Q0[i][1] * Q[i][1]
+        # A[2,1] += Q0[i][1] * Q[i][2]
+
+        # A[0,2] += Q0[i][2] * Q[i][0]
+        # A[1,2] += Q0[i][2] * Q[i][1]
+        # A[2,2] += Q0[i][2] * Q[i][2]
     Apq[None] = A
 
 def calcR_polar():
     # R = AS^-1     S = AA^T ** .5
-    A = Apq[None].value.to_numpy()
-    S = sqrtm(A.T@A)
-    R[None] = ti.Vector(A @ inv(S))
-    print(Rotation.from_matrix(A @ inv(S)).as_quat())
+    try:
+        A = Apq[None].value.to_numpy()
+        S = sqrtm(A.T@A)
+        R[None] = ti.Vector(A @ inv(S))
+    except:
+        R[None] = ti.Vector(quaternion(np.array([0., 0., 0., 1.])))
 
 def angle_axis(direction, angle):
     result = np.array([0., 0., 0., 1.])
-
-    sin = np.sin(angle * .5)
-    cos = np.cos(angle * .5)
+    #angle = np.radians(angle)
+    sin = np.sin(angle/2)
+    cos = np.cos(angle/2)
 
     result[:-1] = direction * sin
-    result[3]  = cos
+    result[3]   = cos
     return result
 
 def quaternion(float4):
     return Rotation.from_quat(float4)
 
+def qmul(q1, q2):
+    # quaternion multiplication
+    x,y,z,w = 0,1,2,3
+    result = np.zeros(4)
+    result[0] = q1[w]*q2[x] + q1[x]*q2[w] + q1[y]*q2[z] - q1[z]*q2[y]  # i
+    result[1] = q1[w]*q2[y] - q1[x]*q2[z] + q1[y]*q2[w] + q1[z]*q2[x]  # j
+    result[2] = q1[w]*q2[z] + q1[x]*q2[y] - q1[y]*q2[x] + q1[z]*q2[w]  # k
+    result[3] = q1[w]*q2[w] - q1[x]*q2[x] - q1[y]*q2[y] - q1[z]*q2[z]  # l
+    return result
 
 def calcR_extract():
     A = Apq[None].value.to_numpy()
-    A = A.T @ Aqq[None].value.to_numpy()
-    q = np.array([0., 0., 0., 1.])
+    R_prev = R[None].value.to_numpy()
+    if norm(R_prev) == 0.:
+        q = np.array([0., 0., 0., 1.])
+    else:
+        q = Rotation.from_matrix(R_prev).as_quat()
 
-    for _ in range(10):
+    for _ in range(3):
         rot = quaternion(q)
         r = rot.as_matrix()
         omega_numerator = np.cross(r[:,0], A[:,0]) + \
-                          np.cross(r[:,1], A[:,2]) + \
+                          np.cross(r[:,1], A[:,1]) + \
                           np.cross(r[:,2], A[:,2])
-        omega_denom = 1. / abs(
+        omega_denom = 1. / (abs(
             np.dot(r[:,0], A[:,0]) + \
             np.dot(r[:,1], A[:,1]) + \
             np.dot(r[:,2], A[:,2])
-        ) + 1.e-9
+        ) + 1.e-9)
         omega = omega_numerator * omega_denom
         w = norm(omega)
         if w < 1.e-9:
             break
-        q = (quaternion(angle_axis(omega/w, w)) * rot).as_quat()
+        q = qmul(angle_axis(omega/w, w), q)
         q = q / norm(q)
-    print(q)
     R[None] = ti.Vector(quaternion(q).as_matrix())
 
 
@@ -216,6 +243,16 @@ def project():
         x, z = x * cp + z * sp, z * cp - x * sp
         u, v = x, y * ct + z * st
         view[i] = ti.Vector([u,v]) * scale[None] + ti.Vector([0.5, 0.5])
+    # focus
+    x,y,z = [0., 0., 0.]
+    x, z = x * cp + z * sp, z * cp - x * sp
+    u, v = x, y * ct + z * st
+    view[12] = ti.Vector([u,v]) * scale[None] + ti.Vector([0.5, 0.5])
+    # cm
+    x,y,z = CM[None] - focus[None]
+    x, z = x * cp + z * sp, z * cp - x * sp
+    u, v = x, y * ct + z * st
+    view[13] = ti.Vector([u,v]) * scale[None] + ti.Vector([0.5, 0.5])
 
 gui = ti.GUI('MPM3D', background_color=0x112F41)
 initCamera()
@@ -257,6 +294,8 @@ while gui.running and not gui.get_event(gui.ESCAPE):
         ALPHA[None] /= 1.01
     if gui.is_pressed('z'):
         method = not method
+        time.sleep(0.3)
+        print(f"RigidSolver: {'RotExt' if method else 'Polar'}")
 
     if gui.is_pressed(ti.GUI.RMB):
         mouse_pos = gui.get_cursor_pos()
@@ -268,8 +307,10 @@ while gui.running and not gui.get_event(gui.ESCAPE):
         attract = 0
 
     
-    gui.circle(focus[None], radius=10*scale[None], color=0xff0000)
-    gui.circles(pos, radius=5*scale[None], color=0x66ccff)
+    gui.circles(pos[:12], radius=5*scale[None], color=0x66ccff)
+    gui.circle(pos[12], radius=1*scale[None], color=0xff0000)
+    gui.circle(pos[13], radius=10*scale[None], color=0xff0000)
+
     gui.lines(pos[edges[0]], pos[edges[1]], color=0x66ccff, radius=1*scale[None])
     gui.show()
 