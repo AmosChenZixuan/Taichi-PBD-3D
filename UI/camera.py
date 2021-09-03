5240d61021dd493a3a9223133d5440d4be65edfe
AmosChenZixuan <czxvvv@foxmail.com>
2021-09-03 16:03:03 +0800

    Update camera.py

--- UI/camera.py
+++ UI/camera.py
@@ -4,15 +4,15 @@ from include.data import *
 
 @ti.data_oriented
 class Camera:
-    SIN0 = ti.sin(0)
-    COS0 = ti.cos(0)
-    CENTER3 = vec3(.5, .5, .5)
-    CENTER2 = vec2(.5, .5)
-
-    def __init__(self, focus=CENTER3, angle=arr2(), scale=1.):
+    def __init__(self, focus=arr3(), angle=arr2(), scale=1.):
         self.angle = field((), 2, ti.f32)
         self.focus = field((), 3, ti.f32)
         self.scale = field((), 1, ti.f32)
+        self.init_state = (focus, angle, scale)
+        self.reset()
+
+    def reset(self):
+        focus, angle, scale = self.init_state
         self.focus[None] = focus
         self.angle[None] = angle
         self.scale[None] = scale
@@ -34,33 +34,29 @@ class Camera:
         return self.scale[None]
 
     def rotate(self, dp, dt):
-        self.angle[None] += dp, dt
+        self.angle[None][0] += dp
+        self.angle[None][1] += dt
         
     def move(self, dx, dy, dz):
-        self.focus[None] += dx, dy, dz
+        self.focus[None][0] += dx
+        self.focus[None][1] += dy
+        self.focus[None][2] += dz
 
     def zoom(self, gamma):
         self.scale[None] *= gamma
 
-    @ti.func
+    @ti.pyfunc
     def project(self, v3):
         p, t = self.getPhi(), self.getTheta()
         cp, sp = ti.cos(p), ti.sin(p)
         ct, st = ti.cos(t), ti.sin(t)
 
-        x,y,z = v3 - self.getFocus()
+        x = v3[0] - self.focus[None][0]
+        y = v3[1] - self.focus[None][1]
+        z = v3[2] - self.focus[None][2]
         x, z = x * cp + z * sp, z * cp - x * sp
         u, v = x, y * ct + z * st
         return vec2(u,v) * self.getScale() + vec2(.5, .5)
-
-    def toTexture(self, v3):
-        cp, sp = self.COS0, self.SIN0
-        ct, st = self.COS0, self.SIN0
-
-        x,y,z = v3 - self.CENTER3
-        x, z = x * cp + z * sp, z * cp - x * sp
-        u, v = x, y * ct + z * st
-        return vec2(u,v) + self.CENTER2
 
 
     
