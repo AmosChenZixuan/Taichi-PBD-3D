d077a871d445d02c9196c61691f4d4ec1a0b0a1a
AmosChenZixuan <czxvvv@foxmail.com>
2021-09-13 15:57:41 +0800

    disable buggy surface volume solver

--- solvers/surfaceVolume.py
+++ solvers/surfaceVolume.py
@@ -7,7 +7,7 @@ from include import *
 
 @ti.data_oriented
 class SurfaceBasedVolumeSolver:
-    def __init__(self, memory: Memory, nParticles, nTris, retStf=1.):
+    def __init__(self, memory: Memory, nParticles, nTris, retStf=1e-6):
         self.mem  = memory
         self.size = nTris
         self.retStf = retStf
@@ -16,7 +16,7 @@ class SurfaceBasedVolumeSolver:
         self.V    = field((), 1, ti.f32)        # curr Volume
         self.V3   = field((), 1, ti.f32)        # rest Volume const; 3 * V0
         self.Tris = field(nTris, 3, ti.i32)     # vertices of triangles
-        # self.norm = field(nTris, 3, ti.f32)     # triangles norms
+        self.norm = field(nTris, 3, ti.f32)     # triangles norms
         # self.A    = field(nTris, 1, ti.f32)     # triangles areas
         self.w    = field(nParticles, 1, ti.f32)  # weights
         self.nSum = field(nParticles, 3, ti.f32)  # sum of the area weighted normals of all triangles containing the i-th particle.
@@ -24,18 +24,18 @@ class SurfaceBasedVolumeSolver:
 
     def reset(self):
         self.K[None]  = self.retStf
-        self.V[None] = 0.
+        self.V[None]  = 0.
         self.V3[None] = 0.
 
     def update(self, i, x, y, z):
         self.Tris[i] = x,y,z
 
     def init(self):
+        self.clearData()
+        self.initNorm()
         self.updateTris()
         self.updateVol()
         self.V3[None] = 3 * self.V[None]
-        print(self.V3[None])
-        t=0
 
     
     #@timeThis
@@ -48,6 +48,14 @@ class SurfaceBasedVolumeSolver:
     ################### Private Methods #####################
     def clearData(self):
         self.nSum.fill(0.)
+
+    @ti.kernel
+    def initNorm(self):
+        mem = self.mem
+        for i in range(self.size):
+            x,y,z     = self.Tris[i]
+            px,py,pz  = mem.newPos[x], mem.newPos[y], mem.newPos[z]
+            self.norm[i] = (py-px).cross(pz-px)
     
 
     @ti.kernel
@@ -56,13 +64,13 @@ class SurfaceBasedVolumeSolver:
         for i in range(self.size):
             x,y,z     = self.Tris[i]
             px,py,pz  = mem.newPos[x], mem.newPos[y], mem.newPos[z]
-            n         = (py-px).cross(pz-px)
+            n         = self.norm[i]
             
             area      = self.calcArea(px,py,pz)
             AN        = area * n 
-            if AN.norm() <= 0. or AN.norm() > .01:
-                print(px,py,pz, n, area)
-                continue
+            # if AN.norm() <= 0. or AN.norm() > .01:
+            #     print(i, x,y,z, n, area)
+            #     continue
             # self.norm[i] = n
             # self.A[i]    = area
             self.nSum[x]+= AN
@@ -79,8 +87,9 @@ class SurfaceBasedVolumeSolver:
         mem = self.mem
         for i in self.nSum:
             self.V[None] += (mem.newPos[i].transpose() @ self.nSum[i])[0,0]
+            #print(i, mem.newPos[i], mem.newPos[i].transpose() @ self.nSum[i])
         self.V[None] /= 3.
-        #print(self.V[None])
+        print(self.V[None])
 
     @ti.kernel
     def project(self):
@@ -101,6 +110,7 @@ class SurfaceBasedVolumeSolver:
 
     @ti.func
     def calcArea(self, x,y,z):
-        a,b,c = (x-y).norm(), (y-z).norm(), (z-x).norm()
-        s     = (a+b+c)/2.
-        return (s*(s-a)*(s-b)*(s-c)) ** 0.5  
+        # a,b,c = (x-y).norm(), (y-z).norm(), (z-x).norm()
+        # s     = (a+b+c)/2.
+        # return (s*(s-a)*(s-b)*(s-c)) ** 0.5  
+        return (y-x).cross(z-x) * .5
