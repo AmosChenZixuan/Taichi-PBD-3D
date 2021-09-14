f61ed157b2a64e9784300d92a77eefa92119a7c9
AmosChenZixuan <czxvvv@foxmail.com>
2021-09-14 11:30:18 +0800

    add demo balloon

--- DemoBalloon.py
+++ DemoBalloon.py
@@ -14,7 +14,6 @@ points, cells = createGalBall(2., surface=True)
 N = len(points)
 triangulation = cells[0].data
 edges         = getEdges(triangulation)
-surfaceVert   = getSurfaceVertices(cells[0].data)
 
 # Allocate memory
 N = N+4   # number of particles; plus one for floor
@@ -26,10 +25,8 @@ edges[1].extend([N-3,N-2,N-1,N-4, N-2])
 camera = Camera(focus=(.5, .5,.5), angle=(5., 1.), scale=.8)
 # volume 
 NC            = len(triangulation)           # number of constraint
-tetSolver   = ClothBalloonSolver(memory, N-4, NC)
+tetSolver     = ClothBalloonSolver(memory, N-4, NC, .5)
 stretchSolver = TotalStretchSolver(memory, N-4, len(edges[0])-5)
-# shape
-shapeSolver = ShapeMatchingSolver(memory, N-4)
 # pbd
 pbd         = PostionBasedDynamics(memory, camera, N)
 
@@ -38,16 +35,11 @@ def init():
     pbd.reset()
     # reset solvers
     tetSolver.reset()
-    shapeSolver.reset()
     stretchSolver.reset()
 
     # mesh
     for i in range(N-4):
         memory.update(i, points[i], 1.)
-        shapeSolver.update(i, i)
-    # for i,j in enumerate(surfaceVert):
-    #     shapeSolver.update(i, j)
-    shapeSolver.init()
 
     # constraint
     for i in range(NC):
@@ -69,7 +61,6 @@ def step(paused, mouse_pos, picked):
     if not paused:
         for _ in range(pbd.substep):
             pbd.apply_force(mouse_pos[0], mouse_pos[1], picked)
-            #shapeSolver.solve()
             for _ in range(pbd.iters[None]):
                 stretchSolver.solve()
                 tetSolver.solve()
@@ -96,10 +87,6 @@ while gui.running:
     eventHandler.fastReact(camera=camera, 
                         pos2d=pos2,
                         pick_method=pick)
-    if gui.is_pressed(","):
-        shapeSolver.ALPHA[None] /= 1.1
-    elif gui.is_pressed('.'):
-        shapeSolver.ALPHA[None] *= 1.1
     if gui.is_pressed("="):
         pbd.ceil[None] = min(pbd.ceil[None]+.01, 1.)
     elif gui.is_pressed('-'):
@@ -113,8 +100,7 @@ while gui.running:
     gui.line([0, pbd.ceil[None]], [1, pbd.ceil[None]],color=0xffeedd, radius=2*scale )
     gui.text(content=f'Stiffness={tetSolver.K[None]}',pos=(0,0.95), color=0xffffff)
     gui.text(content=f'Iteration={pbd.iters[None]}',pos=(0,0.9), color=0xffffff)
-    gui.text(content=f'Shape={shapeSolver.ALPHA[None]}',pos=(0,0.85), color=0xffffff)
-    gui.text(content=f'Ceiling={pbd.ceil[None]}',pos=(0,0.8), color=0xffffff)
+    gui.text(content=f'Ceiling={pbd.ceil[None]}',pos=(0,0.85), color=0xffffff)
     gui.show()
     # sim
     step(eventHandler.paused, eventHandler.mouse_pos, eventHandler.picked)
--- solvers/balloon.py
+++ solvers/balloon.py
@@ -24,14 +24,11 @@ class ClothBalloonSolver:
         self.K    = field((), 1, ti.f32)         # pressure
         self.Tris = field(nTris, 3, ti.i32)      # vertices of triangles
         self.Vol  = field(2, 1, ti.f32)          # [rest volume, cur volume]
-        self.grad = field(memory.size, 3, ti.f32)# constraint gradients
-        self.dp   = field(memory.size, 3, ti.f32)# postion delta  
-        self.w    = field(memory.size, 1, ti.i32)# weights; number of springs on each vertex  
+        self.grad = field(memory.size, 3, ti.f32)# constraint gradients 
 
     def reset(self):
         self.points  = set() 
         self.K[None] = self.rstP
-        self.w.fill(0)  
         self.Vol.fill(0.)
 
     def update(self, i, x, y, z):
@@ -40,28 +37,23 @@ class ClothBalloonSolver:
             if idx not in self.points:
                 self.ptr[len(self.points)] = idx
                 self.points.add(idx)
-        self.w[x] += 16
-        self.w[y] += 16
-        self.w[z] += 16
 
 
     def init(self):
-        self.clearDelta()
+        self.clearCache()
         self.updateVolume()
         self.Vol[0] = self.Vol[1]   # assign rest volume
 
     #@timeThis
     def solve(self):
-        self.clearDelta()
+        self.clearCache()
         self.updateVolume()
         self.updateGradients()
-        self.calcDelta()
-        self.applyDelta()
+        self.project()
 
     ################### Private Methods #####################
 
-    def clearDelta(self):
-        self.dp.fill(0.) 
+    def clearCache(self):
         self.grad.fill(0.)
 
     @ti.kernel
@@ -87,28 +79,18 @@ class ClothBalloonSolver:
             self.grad[z] += px.cross(py)
 
     @ti.kernel
-    def calcDelta(self):
+    def project(self):
         mem = self.mem
-        for i in range(self.size[1]):
-            x, y, z  = self.Tris[i]
-            Cx       = self.Vol[1] - self.Vol[0]*self.K[None]
-            dx,dy,dz = self.grad[x], self.grad[y], self.grad[z]
-
-            gradSum = dx.norm_sqr() * mem.invM[x] + dy.norm_sqr() * mem.invM[y] + \
-                        dz.norm_sqr() * mem.invM[z]
-            if abs(gradSum) > 1e-9:
-                vlambda = Cx / gradSum
-                self.dp[x] -= vlambda * mem.invM[x] * dx
-                self.dp[y] -= vlambda * mem.invM[y] * dy
-                self.dp[z] -= vlambda * mem.invM[z] * dz
-            else:
-                print('WTF')
-
-    @ti.kernel
-    def applyDelta(self):
-        mem = self.mem
+        gradSum = 0.
+        Cx      = self.Vol[1] - self.Vol[0]*self.K[None]
         for i in range(self.size[0]):
             x = self.ptr[i]
-            mem.newPos[x] += self.dp[x] / self.w[x]**2
+            gradSum += self.grad[x].norm_sqr() * mem.invM[x]
+        for i in range(self.size[0]):
+            if abs(gradSum) < 1e-9:
+                print('WTF')
+                break
+            x = self.ptr[i]
+            mem.newPos[x] += -(Cx / gradSum) * mem.invM[x] * self.grad[x]
 
     
